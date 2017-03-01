664ed6570286ad418d4ec896df6dec36ccdf5e7d
Muggle Wei <mugglewei@gmail.com>
2017-03-01 23:35:02 +0800

    Modify directory structure, split all codes into 4 lib

--- cmake/add_project.cmake
+++ cmake/add_project.cmake
@@ -52,10 +52,10 @@ function(addMuggleLib name is_pure_c export_macro folder_name link_targets depen
 
 	# windows dll export and import
 	if(MSVC AND ${MUGGLE_BUILD_SHARED_LIB})
-	target_compile_definitions(${name}
-		PUBLIC MUGGLE_USE_DLLS
-		PRIVATE ${export_macro} 
-	)
+		target_compile_definitions(${name}
+			PUBLIC MUGGLE_USE_DLLS
+			PRIVATE ${export_macro} 
+		)
 	endif()
 
 	# install
@@ -67,6 +67,105 @@ function(addMuggleLib name is_pure_c export_macro folder_name link_targets depen
 	)
 
 endfunction(addMuggleLib)
+
+# add muggle library in folder recusively
+function(addMuggleLibRecurs name is_pure_c export_macro folder_name link_targets dependencies)
+
+	# files
+	file(GLOB_RECURSE  ${name}_h ${muggle_source_dir}/muggle/${folder_name}/*.h)
+	file(GLOB_RECURSE  ${name}_c ${muggle_source_dir}/muggle/${folder_name}/*.c)
+	file(GLOB_RECURSE  ${name}_cpp ${muggle_source_dir}/muggle/${folder_name}/*.cpp)
+	
+	if (${is_pure_c})
+		file(GLOB ${name}_src 
+			${${name}_c}
+		)
+	else()
+		file(GLOB ${name}_src 
+			${${name}_c}
+			${${name}_cpp}
+		)
+	endif()
+	
+	# add lib
+	if (WIN32)
+		add_library(${name} ${MUGGLE_LIB_TYPE}
+			${${name}_h}
+			${${name}_src}
+		)
+	else()
+		add_library(${name} ${MUGGLE_LIB_TYPE}
+			${${name}_src}
+		)
+	endif()
+
+	# property
+	if (${is_pure_c})
+		set_target_properties(${name} PROPERTIES 
+			LINKER_LANGUAGE C
+		)
+	endif()
+	
+	# link
+	if (NOT link_targets STREQUAL "")
+		target_link_libraries(${name} 
+			${link_targets}
+		)
+	endif()
+
+	# dependencies
+	if (NOT dependencies STREQUAL "")
+		add_dependencies(${name}
+			${dependencies}
+		)
+	endif()
+
+	# windows dll export and import
+	if(MSVC AND ${MUGGLE_BUILD_SHARED_LIB})
+		target_compile_definitions(${name}
+			PUBLIC MUGGLE_USE_DLLS
+			PRIVATE ${export_macro} 
+		)
+	endif()
+
+	# put all directories and files into list
+	set(${name}_all_files ${${name}_src} ${${name}_h})
+	foreach(file ${${name}_all_files})
+		file(RELATIVE_PATH rel_path ${muggle_source_dir}/muggle/${folder_name} ${file})
+		get_filename_component(file_dir ${rel_path} DIRECTORY)
+		if (NOT ${file_dir} EQUAL "")
+			list(FIND ${name}_all_dirs ${file_dir} idx)
+			if (${idx} EQUAL -1)
+				list(APPEND ${name}_all_dirs ${file_dir})
+			endif()
+			list(APPEND ${file_dir}_src ${file})
+
+			if (${file} MATCHES ".h$")
+				list(APPEND ${file_dir}_h ${file})
+			endif()
+		else()
+			if (${file} MATCHES ".h$")
+				list(APPEND empty_dir_h ${file})
+			endif()
+		endif()
+	endforeach()
+
+	# source group and install head
+	foreach(dir ${${name}_all_dirs})
+		string(REPLACE "/" "\\" GROUP "${dir}")
+		source_group(${GROUP} FILES ${${dir}_src})
+		install(FILES ${${name}_h} DESTINATION ${MUGGLE_INSTALL_INCLUDE_PATH}/${folder_name}/${dir})
+	endforeach()
+	install(FILES ${empty_dir_h} DESTINATION ${MUGGLE_INSTALL_INCLUDE_PATH}/${folder_name})
+
+	# install binary files
+	install(TARGETS ${name}
+		RUNTIME DESTINATION ${MUGGLE_INSTALL_RUNTIME_PATH}
+		LIBRARY DESTINATION ${MUGGLE_INSTALL_LIBRARY_PATH}
+		ARCHIVE DESTINATION ${MUGGLE_INSTALL_ARCHIVE_PATH}
+	)	
+
+endfunction(addMuggleLibRecurs)
 
 # add simple project
 function(addProject name folder_name dependencies type)
