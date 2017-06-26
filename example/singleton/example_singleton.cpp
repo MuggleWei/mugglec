788d78f120e85ce13ea8d8359d1221d44567f476
Muggle Wei <mugglewei@gmail.com>
2017-06-26 20:24:25 +0800

    add singleton

---
+++ example/singleton/example_singleton.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "muggle/utils_cpp/design_pattern/singleton.hpp"
+
+class A
+{
+public:
+	A()
+		: i_(0)
+	{
+		std::cout << "A()" << std::endl;
+	}
+	A(int i)
+		: i_(i)
+	{
+		std::cout << "A(i)" << std::endl;
+	}
+
+	void print()
+	{
+		std::cout << i_ << std::endl;
+	}
+
+private:
+	int i_;
+};
+
+class ResManager : public muggle::Singleton<ResManager>
+{
+public:
+	ResManager(int i = 0)
+		: res_(i)
+	{}
+	void Init(int res)
+	{
+		res_ = res;
+	}
+	void print()
+	{
+		std::cout << res_ << std::endl;
+	}
+
+private:
+	int res_;
+};
+
+int main(int argc, char *argv[])
+{
+	muggle::Singleton<A>::Instance(5);
+	muggle::Singleton<A>::Instance().print();
+
+	ResManager::Instance(1);
+	ResManager::Instance().Init(5);
+	ResManager::Instance().print();
+
+	return 0;
+}
--- muggle/utils_cpp/utils_cpp.h
+++ muggle/utils_cpp/utils_cpp.h
@@ -10,14 +10,8 @@
 
 #include "muggle/base_c/base_c.h"
 #include "muggle/utils_cpp/utils_cpp_macro.h"
-
-#ifdef USE_MUGGLE_TIME
 #include "muggle/utils_cpp/time/delta_time.h"
-#endif
-
-#ifdef USE_MUGGLE_MEMORY_DETECT
 #include "muggle/utils_cpp/mem_detect/mem_detect.h"
-#endif
-
+#include "muggle/utils_cpp/design_pattern/singleton.hpp"
 
 #endif
