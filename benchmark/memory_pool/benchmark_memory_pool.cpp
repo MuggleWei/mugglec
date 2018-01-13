91dd9106f89627797b8c4b266304cf4e9b08d137
Muggle Wei <mugglewei@gmail.com>
2018-01-13 14:46:22 +0800

    add rand operation in memory pool benchmark

--- benchmark/memory_pool/benchmark_memory_pool.cpp
+++ benchmark/memory_pool/benchmark_memory_pool.cpp
@@ -23,6 +23,7 @@
 typedef std::chrono::duration<double, std::milli> double_dura;
 typedef std::function<void(intptr_t*, int, unsigned int)> allocFunc;
 typedef std::function<void(intptr_t*, int)> freeFunc;
+typedef std::function<void(intptr_t*, int, unsigned int, int*)> randFunc;
 
 double invokeAlloc(
 	intptr_t *buf, int times, unsigned int block_size,
@@ -46,6 +47,17 @@ double invokeFree(intptr_t* buf, int times, freeFunc &func)
 
 	return std::chrono::duration_cast<double_dura>(end - start).count();
 }
+double invokeRand(intptr_t *buf, int times, unsigned int block_size,
+	int* op_flag, randFunc &func)
+{
+	auto start = std::chrono::high_resolution_clock::now();
+
+	func(buf, times, block_size, op_flag);
+
+	auto end = std::chrono::high_resolution_clock::now();
+
+	return std::chrono::duration_cast<double_dura>(end - start).count();
+}
 
 void originAlloc(intptr_t *buf, int times, unsigned int block_size)
 {
@@ -57,6 +69,26 @@ void originAlloc(intptr_t *buf, int times, unsigned int block_size)
 void originFree(intptr_t* buf, int times)
 {
 	for (int i = 0; i < times; ++i)
+	{
+		free((void*)buf[i]);
+	}
+}
+void originRand(intptr_t *buf, int times, unsigned int block_size, int *op_flag)
+{
+	int idx = 0;
+	for (int i = 0; i < times; ++i)
+	{
+		if (op_flag[i] == 1)
+		{
+			buf[idx++] = (intptr_t)malloc(block_size);
+		}
+		else
+		{
+			free((void*)buf[--idx]);
+		}
+	}
+
+	for (int i = 0; i < idx; ++i)
 	{
 		free((void*)buf[i]);
 	}
@@ -76,6 +108,26 @@ void memorypoolFree(muggle::MemoryPool &pool, intptr_t* buf, int times)
 		pool.recycle((void*)buf[i]);
 	}
 }
+void memorypoolRand(muggle::MemoryPool &pool, intptr_t *buf, int times, unsigned int block_size, int *op_flag)
+{
+	int idx = 0;
+	for (int i = 0; i < times; ++i)
+	{
+		if (op_flag[i] == 1)
+		{
+			buf[idx++] = (intptr_t)pool.alloc();
+		}
+		else
+		{
+			pool.recycle((void*)buf[--idx]);
+		}
+	}
+
+	for (int i = 0; i < idx; ++i)
+	{
+		pool.recycle((void*)buf[i]);
+	}
+}
 
 void threadsafeMemorypoolAlloc(muggle::ThreadSafeMemoryPool &pool, intptr_t *buf, int times, unsigned int block_size)
 {
@@ -91,22 +143,52 @@ void threadsafeMemorypoolFree(muggle::ThreadSafeMemoryPool &pool, intptr_t* buf,
 		pool.recycle((void*)buf[i]);
 	}
 }
+void threadsafeMemorypoolRand(muggle::ThreadSafeMemoryPool &pool, intptr_t *buf, int times, unsigned int block_size, int *op_flag)
+{
+	int idx = 0;
+	for (int i = 0; i < times; ++i)
+	{
+		if (op_flag[i] == 1)
+		{
+			buf[idx++] = (intptr_t)pool.alloc();
+		}
+		else
+		{
+			pool.recycle((void*)buf[--idx]);
+		}
+	}
+
+	for (int i = 0; i < idx; ++i)
+	{
+		pool.recycle((void*)buf[i]);
+	}
+}
 
 
-void benchmarkOrigin(int times, unsigned int block_size, double &alloc_ms, double &free_ms)
+void benchmarkOrigin(int times, unsigned int block_size, 
+	double &alloc_ms, double &free_ms, double &rand_ms, int *op_flag)
 {
 	intptr_t *buf = (intptr_t*)malloc(sizeof(void*) * times);
 
-	allocFunc alloc_func = std::bind(&originAlloc, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+	allocFunc alloc_func = std::bind(&originAlloc, 
+		std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
 	alloc_ms = invokeAlloc(buf, times, block_size, alloc_func);
 
-	freeFunc free_func = std::bind(&originFree, std::placeholders::_1, std::placeholders::_2);
+	freeFunc free_func = std::bind(&originFree, 
+		std::placeholders::_1, std::placeholders::_2);
 	free_ms = invokeFree(buf, times, free_func);
+
+	randFunc rand_func = std::bind(&originRand, 
+		std::placeholders::_1, std::placeholders::_2, 
+		std::placeholders::_3, std::placeholders::_4);
+	rand_ms = invokeRand(buf, times, block_size, op_flag, rand_func);
 
 	free(buf);
 }
 
-void benchmarkMemoryPool(muggle::MemoryPool &pool, int times, unsigned int block_size, double &alloc_ms, double &free_ms)
+void benchmarkMemoryPool(muggle::MemoryPool &pool, int times, 
+	unsigned int block_size, double &alloc_ms, double &free_ms, 
+	double &rand_ms, int *op_flag)
 {
 	intptr_t *buf = (intptr_t*)malloc(sizeof(void*) * times);
 
@@ -118,10 +200,17 @@ void benchmarkMemoryPool(muggle::MemoryPool &pool, int times, unsigned int block
 		std::placeholders::_1, std::placeholders::_2);
 	free_ms = invokeFree(buf, times, free_func);
 
+	randFunc rand_func = std::bind(&memorypoolRand, std::ref(pool),
+		std::placeholders::_1, std::placeholders::_2,
+		std::placeholders::_3, std::placeholders::_4);
+	rand_ms = invokeRand(buf, times, block_size, op_flag, rand_func);
+
 	free(buf);
 }
 
-void benchmarkThreadsafeMemoryPool(muggle::ThreadSafeMemoryPool &pool, int times, unsigned int block_size, double &alloc_ms, double &free_ms)
+void benchmarkThreadsafeMemoryPool(muggle::ThreadSafeMemoryPool &pool, 
+	int times, unsigned int block_size, double &alloc_ms, double &free_ms, 
+	double &rand_ms, int *op_flag)
 {
 	intptr_t *buf = (intptr_t*)malloc(sizeof(void*) * times);
 
@@ -133,6 +222,11 @@ void benchmarkThreadsafeMemoryPool(muggle::ThreadSafeMemoryPool &pool, int times
 		std::placeholders::_1, std::placeholders::_2);
 	free_ms = invokeFree(buf, times, free_func);
 
+	randFunc rand_func = std::bind(&threadsafeMemorypoolRand, std::ref(pool),
+		std::placeholders::_1, std::placeholders::_2,
+		std::placeholders::_3, std::placeholders::_4);
+	rand_ms = invokeRand(buf, times, block_size, op_flag, rand_func);
+
 	free(buf);
 }
 
@@ -143,10 +237,13 @@ unsigned int block_size[] = { 8, 16, 32, 64, 128, 512, 1024, 2048, 4096, 10240, 
 const int len_time = sizeof(times) / sizeof(times[0]);
 const int len_block_size = sizeof(block_size) / sizeof(block_size[0]);
 
+int *op_flag[len_time] = { nullptr };	// operation flags: 1 - malloc, -1 - free
+
 struct Records
 {
 	double alloc_records[len_time][len_block_size];
 	double free_records[len_time][len_block_size];
+	double rand_records[len_time][len_block_size];
 };
 
 void writeToFile(const std::string& name, double *records)
@@ -206,6 +303,7 @@ void benchmarkSingleThread()
 {
 	double alloc_records[len_time][len_block_size] = {};
 	double free_records[len_time][len_block_size] = {};
+	double rand_records[len_time][len_block_size] = {};
 
 	// origin
 	for (int i = 0; i < len_time; ++i)
@@ -213,11 +311,12 @@ void benchmarkSingleThread()
 		for (int j = 0; j < len_block_size; ++j)
 		{
 			std::cout << "single thread - malloc/free - " << times[i] << ", " << block_size[j] << std::endl;
-			benchmarkOrigin(times[i], block_size[j], alloc_records[i][j], free_records[i][j]);
+			benchmarkOrigin(times[i], block_size[j], alloc_records[i][j], free_records[i][j], rand_records[i][j], op_flag[i]);
 		}
 	}
 	writeToFile("origin_alloc", &alloc_records[0][0]);
 	writeToFile("origin_free", &free_records[0][0]);
+	writeToFile("origin_rand", &rand_records[0][0]);
 
 	// pool
 	for (int i = 0; i < len_time; ++i)
@@ -227,11 +326,13 @@ void benchmarkSingleThread()
 			std::cout << "single thread - pool - " << times[i] << ", " << block_size[j] << std::endl;
 			muggle::MemoryPool pool(times[i], block_size[j]);
 			shufflePool<muggle::MemoryPool>(pool);
-			benchmarkMemoryPool(pool, times[i], block_size[j], alloc_records[i][j], free_records[i][j]);
+			benchmarkMemoryPool(pool, times[i], block_size[j], alloc_records[i][j], 
+				free_records[i][j], rand_records[i][j], op_flag[i]);
 		}
 	}
 	writeToFile("pool_alloc", &alloc_records[0][0]);
 	writeToFile("pool_free", &free_records[0][0]);
+	writeToFile("pool_rand", &rand_records[0][0]);
 
 	// thread safe pool
 	for (int i = 0; i < len_time; ++i)
@@ -241,14 +342,16 @@ void benchmarkSingleThread()
 			std::cout << "single thread - thread safe pool - " << times[i] << ", " << block_size[j] << std::endl;
 			muggle::ThreadSafeMemoryPool pool(times[i], block_size[j]);
 			shufflePool<muggle::ThreadSafeMemoryPool>(pool);
-			benchmarkThreadsafeMemoryPool(pool, times[i], block_size[j], alloc_records[i][j], free_records[i][j]);
+			benchmarkThreadsafeMemoryPool(pool, times[i], block_size[j], alloc_records[i][j], 
+				free_records[i][j], rand_records[i][j], op_flag[i]);
 		}
 	}
 	writeToFile("threadsafe_pool_alloc", &alloc_records[0][0]);
 	writeToFile("threadsafe_pool_free", &free_records[0][0]);
+	writeToFile("threadsafe_pool_rand", &rand_records[0][0]);
 }
 
-void accuRecords(std::vector<Records> &records, double *alloc_records, double *free_records)
+void accuRecords(std::vector<Records> &records, double *alloc_records, double *free_records, double *rand_records)
 {
 	for (auto &rec : records)
 	{
@@ -258,6 +361,7 @@ void accuRecords(std::vector<Records> &records, double *alloc_records, double *f
 			{
 				*(alloc_records + i * len_block_size + j) += rec.alloc_records[i][j];
 				*(free_records + i * len_block_size + j) += rec.free_records[i][j];
+				*(rand_records + i * len_block_size + j) += rec.rand_records[i][j];
 			}
 		}
 	}
@@ -267,6 +371,7 @@ void benchmarkMultipleThread()
 {
 	double alloc_records[len_time][len_block_size] = {};
 	double free_records[len_time][len_block_size] = {};
+	double rand_records[len_time][len_block_size] = {};
 
 	unsigned int thread_nums = std::thread::hardware_concurrency();
 	thread_nums = thread_nums > 1 ? thread_nums : 2;
@@ -279,6 +384,7 @@ void benchmarkMultipleThread()
 	records.clear();
 	memset(&alloc_records[0][0], 0, sizeof(double) * len_time * len_block_size);
 	memset(&free_records[0][0], 0, sizeof(double) * len_time * len_block_size);
+	memset(&rand_records[0][0], 0, sizeof(double) * len_time * len_block_size);
 	for (int i = 0; i < len_time; ++i)
 	{
 		for (int j = 0; j < len_block_size; ++j)
@@ -290,7 +396,8 @@ void benchmarkMultipleThread()
 				futures.push_back(std::async(std::launch::async, [&i, &j]()->Records {
 					Records rec;
 					memset(&rec, 0, sizeof(rec));
-					benchmarkOrigin(times[i], block_size[j], rec.alloc_records[i][j], rec.free_records[i][j]);
+					benchmarkOrigin(times[i], block_size[j], rec.alloc_records[i][j], 
+						rec.free_records[i][j], rec.rand_records[i][j], op_flag[i]);
 					return rec;
 				}));
 			}
@@ -301,7 +408,7 @@ void benchmarkMultipleThread()
 			futures.clear();
 		}
 	}
-	accuRecords(records, &alloc_records[0][0], &free_records[0][0]);
+	accuRecords(records, &alloc_records[0][0], &free_records[0][0], &rand_records[0][0]);
 
 	memset(name_buf, 0, sizeof(name_buf));
 	snprintf(name_buf, 63, "mul_th_%d_origin_alloc", thread_nums);
@@ -311,10 +418,15 @@ void benchmarkMultipleThread()
 	snprintf(name_buf, 63, "mul_th_%d_origin_free", thread_nums);
 	writeToFile(name_buf, &free_records[0][0]);
 
+	memset(name_buf, 0, sizeof(name_buf));
+	snprintf(name_buf, 63, "mul_th_%d_origin_rand", thread_nums);
+	writeToFile(name_buf, &rand_records[0][0]);
+
 	// thread safe pool
 	records.clear();
 	memset(&alloc_records[0][0], 0, sizeof(double) * len_time * len_block_size);
 	memset(&free_records[0][0], 0, sizeof(double) * len_time * len_block_size);
+	memset(&rand_records[0][0], 0, sizeof(double) * len_time * len_block_size);
 	for (int i = 0; i < len_time; ++i)
 	{
 		for (int j = 0; j < len_block_size; ++j)
@@ -329,7 +441,8 @@ void benchmarkMultipleThread()
 				futures.push_back(std::async(std::launch::async, [&i, &j, &pool]()->Records {
 					Records rec;
 					memset(&rec, 0, sizeof(rec));
-					benchmarkThreadsafeMemoryPool(pool, times[i], block_size[j], rec.alloc_records[i][j], rec.free_records[i][j]);
+					benchmarkThreadsafeMemoryPool(pool, times[i], block_size[j], rec.alloc_records[i][j], 
+						rec.free_records[i][j], rec.rand_records[i][j], op_flag[i]);
 					return rec;
 				}));
 			}
@@ -340,7 +453,7 @@ void benchmarkMultipleThread()
 			futures.clear();
 		}
 	}
-	accuRecords(records, &alloc_records[0][0], &free_records[0][0]);
+	accuRecords(records, &alloc_records[0][0], &free_records[0][0], &rand_records[0][0]);
 
 	memset(name_buf, 0, sizeof(name_buf));
 	snprintf(name_buf, 63, "mul_th_%d_threadsafe_pool_alloc", thread_nums);
@@ -350,10 +463,15 @@ void benchmarkMultipleThread()
 	snprintf(name_buf, 63, "mul_th_%d_threadsafe_pool_free", thread_nums);
 	writeToFile(name_buf, &free_records[0][0]);
 
+	memset(name_buf, 0, sizeof(name_buf));
+	snprintf(name_buf, 63, "mul_th_%d_threadsafe_pool_rand", thread_nums);
+	writeToFile(name_buf, &rand_records[0][0]);
+
 	// pool with thread local
 	records.clear();
 	memset(&alloc_records[0][0], 0, sizeof(double) * len_time * len_block_size);
 	memset(&free_records[0][0], 0, sizeof(double) * len_time * len_block_size);
+	memset(&rand_records[0][0], 0, sizeof(double) * len_time * len_block_size);
 	for (int i = 0; i < len_time; ++i)
 	{
 		for (int j = 0; j < len_block_size; ++j)
@@ -369,7 +487,8 @@ void benchmarkMultipleThread()
 					muggle::ThreadSafeMemoryPool pool(times[i], block_size[j]);
 					shufflePool<muggle::ThreadSafeMemoryPool>(pool);
 
-					benchmarkThreadsafeMemoryPool(pool, times[i], block_size[j], rec.alloc_records[i][j], rec.free_records[i][j]);
+					benchmarkThreadsafeMemoryPool(pool, times[i], block_size[j], rec.alloc_records[i][j], 
+						rec.free_records[i][j], rec.rand_records[i][j], op_flag[i]);
 					return rec;
 				}));
 			}
@@ -380,7 +499,7 @@ void benchmarkMultipleThread()
 			futures.clear();
 		}
 	}
-	accuRecords(records, &alloc_records[0][0], &free_records[0][0]);
+	accuRecords(records, &alloc_records[0][0], &free_records[0][0], &rand_records[0][0]);
 
 	memset(name_buf, 0, sizeof(name_buf));
 	snprintf(name_buf, 63, "mul_th_%d_pool_thread_local_alloc", thread_nums);
@@ -389,15 +508,59 @@ void benchmarkMultipleThread()
 	memset(name_buf, 0, sizeof(name_buf));
 	snprintf(name_buf, 63, "mul_th_%d_pool_thread_local_free", thread_nums);
 	writeToFile(name_buf, &free_records[0][0]);
+
+	memset(name_buf, 0, sizeof(name_buf));
+	snprintf(name_buf, 63, "mul_th_%d_pool_thread_local_rand", thread_nums);
+	writeToFile(name_buf, &rand_records[0][0]);
 }
 
+void genOpFlag()
+{
+	for (int i = 0; i < len_time; ++i)
+	{
+		op_flag[i] = (int*)malloc(sizeof(int) * times[i]);
+		int sum = 0;
+		for (int j = 0; j < times[i]; ++j)
+		{
+			int k = rand() % 2;
+			if (k == 0)
+			{
+				k = -1;
+			}
+
+			if (k == -1 && sum + k > 0)
+			{
+				k = -1;
+			}
+			else
+			{
+				k = 1;
+			}
+
+			op_flag[i][j] = k;
+			sum += k;
+		}
+	}
+}
+
+void delOpFlag()
+{
+	for (int i = 0; i < len_time; ++i)
+	{
+		free(op_flag[i]);
+	}
+}
 
 void run()
 {
 	srand(time(nullptr));
 
+	genOpFlag();
+
 	benchmarkSingleThread();
 	benchmarkMultipleThread();
+
+	delOpFlag();
 }
 
 int main()
