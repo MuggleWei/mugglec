225b13cf20634e427bef614f4c513102191273c6
Muggle Wei <mugglewei@gmail.com>
2020-02-23 10:45:39 +0800

    update example time client

--- example/net_time_client/time_client_handle.c
+++ example/net_time_client/time_client_handle.c
@@ -1,49 +1,10 @@
 #include "time_client_handle.h"
 
-static void on_read(struct muggle_socket_event *ev, struct muggle_socket_peer *peer, muggle_bytes_buffer_t *bytes_buf)
+#define UDP_MESSAGE_MAX_SIZE 65536
+
+static inline void on_timestr(struct muggle_socket_event *ev, struct muggle_socket_peer *peer, char *msg)
 {
-	while (1)
-	{
-		uint32_t len;
-		if (!muggle_bytes_buffer_fetch(bytes_buf, 4, &len))
-		{
-			// failed get length information
-			break;
-		}
-
-		// in this example, time string is yyyy-mm-dd hh:MM:ss, 65536 is enough
-		len = ntohl(len);
-		int pkg_size = 4 + (int)len;
-		char tmp_buf[65536];
-		if (pkg_size >= sizeof(tmp_buf))
-		{
-			MUGGLE_ERROR("time string too long, must something wrong");
-			muggle_socket_shutdown(peer->fd, MUGGLE_SOCKET_SHUT_RDWR);
-			return;
-		}
-
-		// get readable number of bytes
-		int readable = muggle_bytes_buffer_readable(bytes_buf);
-		if (readable < pkg_size)
-		{
-			// wait for more bytes from network
-			break;
-		}
-
-		// parse package
-		char *ptr = muggle_bytes_buffer_reader_fc(bytes_buf, pkg_size);
-		if (ptr)
-		{
-			MUGGLE_INFO("recv timestamp string: %s", (char*)(ptr + 4));
-			muggle_bytes_buffer_reader_move(bytes_buf, pkg_size);
-		}
-		else
-		{
-			// there have no contiguous for this package
-			muggle_bytes_buffer_read(bytes_buf, pkg_size, tmp_buf);
-			MUGGLE_INFO("recv timestamp string: %s", (char*)(tmp_buf + 4));
-		}
-	}
+	MUGGLE_INFO("recv timestamp string: %s", msg);
 }
 
 int on_error(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
@@ -67,54 +28,146 @@ int on_error(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
 
 int on_message(struct muggle_socket_event *ev, struct muggle_socket_peer *peer)
 {
-	int bufsize = 65536;
-	muggle_bytes_buffer_t *bytes_buf = (muggle_bytes_buffer_t*)peer->data;
-	char *buf = (char*)muggle_bytes_buffer_writer_fc(bytes_buf, bufsize);
-	if (buf == NULL)
+	int bufsize = UDP_MESSAGE_MAX_SIZE;
+	char buf[UDP_MESSAGE_MAX_SIZE];
+	if (peer->peer_type == MUGGLE_SOCKET_PEER_TYPE_UDP_PEER)
 	{
-		MUGGLE_ERROR("bytes buffer full!");
-		return -1;
-	}
-	while (1)
-	{
-		int n = recv(peer->fd, buf, bufsize, 0);
-		if (n > 0)
+		while (1)
 		{
-			if (!muggle_bytes_buffer_writer_move(bytes_buf, n))
+			int n = recv(peer->fd, buf, sizeof(buf), 0);
+			if (n > 0)
 			{
-				MUGGLE_ERROR("bytes buffer inner error!");
+				if (n < 4)
+				{
+					MUGGLE_ERROR("invalid message with number of bytes < 4");
+					return 0;
+				}
+
+				uint32_t len = ntohl(*(uint32_t*)buf);
+				if (len > UDP_MESSAGE_MAX_SIZE - 4)
+				{
+					MUGGLE_ERROR("length field in message too long");
+					return 0;
+				}
+
+				on_timestr(ev, peer, buf + 4);
+			}
+			else if (n < 0)
+			{
+				if (MUGGLE_SOCKET_LAST_ERRNO == MUGGLE_SYS_ERRNO_INTR)
+				{
+					continue;
+				}
+				else if (MUGGLE_SOCKET_LAST_ERRNO == MUGGLE_SYS_ERRNO_WOULDBLOCK)
+				{
+					break;
+				}
+
+				return -1;
+			}
+			else
+			{
+				return -1;
+			}
+		}
+	}
+	else if (peer->peer_type == MUGGLE_SOCKET_PEER_TYPE_TCP_PEER)
+	{
+		muggle_bytes_buffer_t *bytes_buf = (muggle_bytes_buffer_t*)peer->data;
+		while (1)
+		{
+			// find contiguous memory
+			char *ptr = (char*)muggle_bytes_buffer_writer_fc(bytes_buf, bufsize);
+			if (ptr == NULL)
+			{
+				MUGGLE_ERROR("bytes buffer full!");
 				return -1;
 			}
 
-			if (peer->peer_type == MUGGLE_SOCKET_PEER_TYPE_UDP_PEER)
+			int n = recv(peer->fd, ptr, bufsize, 0);
+			if (n > 0)
 			{
-				continue;
+				if (!muggle_bytes_buffer_writer_move(bytes_buf, n))
+				{
+					MUGGLE_ERROR("bytes buffer inner error!");
+					return -1;
+				}
+
+				if (n < bufsize)
+				{
+					break;
+				}
 			}
-			else if (n < bufsize)
+			else if (n < 0)
 			{
-				break;
+				if (MUGGLE_SOCKET_LAST_ERRNO == MUGGLE_SYS_ERRNO_INTR)
+				{
+					continue;
+				}
+				else if (MUGGLE_SOCKET_LAST_ERRNO == MUGGLE_SYS_ERRNO_WOULDBLOCK)
+				{
+					break;
+				}
+
+				return -1;
+			}
+			else
+			{
+				return -1;
 			}
 		}
-		else if (n < 0)
+
+		while (1)
 		{
-			if (MUGGLE_SOCKET_LAST_ERRNO == MUGGLE_SYS_ERRNO_INTR)
+			uint32_t len;
+			if (!muggle_bytes_buffer_fetch(bytes_buf, 4, &len))
 			{
-				continue;
-			}
-			else if (MUGGLE_SOCKET_LAST_ERRNO == MUGGLE_SYS_ERRNO_WOULDBLOCK)
-			{
+				// failed get length information
 				break;
 			}
 
-			return -1;
-		}
-		else
-		{
-			return -1;
+			// in this example, time string is yyyy-mm-dd hh:MM:ss, 65536 is enough
+			len = ntohl(len);
+			if (len > UDP_MESSAGE_MAX_SIZE - 4)
+			{
+				MUGGLE_ERROR("time string too long, must something wrong");
+				// shutdown and return 0, or only return -1 both is ok
+				muggle_socket_shutdown(peer->fd, MUGGLE_SOCKET_SHUT_RDWR);
+				return 0;
+			}
+
+			// get readable number of bytes
+			int pkg_size = len + 4;
+			int readable = muggle_bytes_buffer_readable(bytes_buf);
+			if (readable < pkg_size)
+			{
+				// wait for more bytes from network
+				break;
+			}
+
+			// parse package
+			char *ptr = muggle_bytes_buffer_reader_fc(bytes_buf, pkg_size);
+			if (ptr)
+			{
+				on_timestr(ev, peer, ptr + 4);
+				if (!muggle_bytes_buffer_reader_move(bytes_buf, pkg_size))
+				{
+					MUGGLE_ERROR("failed buffer reader move");
+					return -1;
+				}
+			}
+			else
+			{
+				// there have no contiguous for this package
+				if (!muggle_bytes_buffer_read(bytes_buf, pkg_size, buf))
+				{
+					MUGGLE_ERROR("failed buffer read");
+					return -1;
+				}
+				on_timestr(ev, peer, buf + 4);
+			}
 		}
 	}
-
-	on_read(ev, peer, bytes_buf);
 
 	return 0;
 }
