51a902b1dbe0b01e922360a1ef1a7de23ab6cbe2
mugglewei <mugglewei@gmail.com>
2020-08-05 17:35:41 +0800

    update unittest_pointer_slot

--- test/pointer_slot/unittest_pointer_slot.cpp
+++ test/pointer_slot/unittest_pointer_slot.cpp
@@ -39,3 +39,35 @@ TEST(pointer_slot, insert_remove)
 
 	muggle_pointer_slot_destroy(&ptr_slot);
 }
+
+TEST(pointer_slot, idx_truncate)
+{
+	unsigned int capacity = 4;
+	muggle_pointer_slot_t ptr_slot;
+	int ret = muggle_pointer_slot_init(&ptr_slot, capacity);
+	ASSERT_EQ(ret, 0);
+
+	ptr_slot.alloc_index = ~(unsigned int)0;
+	ptr_slot.free_index = ptr_slot.alloc_index;
+
+	unsigned int idx, start_idx;
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	ret = muggle_pointer_slot_insert(&ptr_slot, &arr[0], &idx);
+	ASSERT_EQ(ret, 0);
+	start_idx = idx;
+
+	ret = muggle_pointer_slot_insert(&ptr_slot, &arr[1], &idx);
+	ASSERT_EQ(ret, 0);
+	ASSERT_EQ(IDX_IN_POW_OF_2_RING(start_idx + 1, capacity), IDX_IN_POW_OF_2_RING(idx, capacity));
+
+	ret = muggle_pointer_slot_insert(&ptr_slot, &arr[2], &idx);
+	ASSERT_EQ(ret, 0);
+	ASSERT_EQ(IDX_IN_POW_OF_2_RING(start_idx + 2, capacity), IDX_IN_POW_OF_2_RING(idx, capacity));
+
+	ret = muggle_pointer_slot_insert(&ptr_slot, &arr[3], &idx);
+	ASSERT_EQ(ret, 0);
+	ASSERT_EQ(IDX_IN_POW_OF_2_RING(start_idx + 3, capacity), IDX_IN_POW_OF_2_RING(idx, capacity));
+
+	muggle_pointer_slot_destroy(&ptr_slot);
+}
