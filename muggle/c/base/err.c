640a9268b3d8c7850dad877e8bac825b43141617
mugglewei <mugglewei@gmail.com>
2022-06-30 15:07:44 +0800

    update base/err, add sys_lasterror and sys_strerror

---
+++ muggle/c/base/err.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *  @file         err.c
+ *  @author       Muggle Wei
+ *  @email        mugglewei@gmail.com
+ *  @date         2022-06-30
+ *  @copyright    Copyright 2022 Muggle Wei
+ *  @license      MIT License
+ *  @brief        mugglec err message
+ *****************************************************************************/
+
+#include "err.h"
+#include <errno.h>
+#include <string.h>
+
+#if MUGGLE_PLATFORM_WINDOWS
+#include <errhandlingapi.h>
+#endif
+
+int muggle_sys_lasterror()
+{
+#if MUGGLE_PLATFORM_WINDOWS
+	return GetLastError();
+#else
+	return errno;
+#endif
+}
+
+int muggle_sys_strerror(int errnum, char *buf, size_t bufsize)
+{
+#if MUGGLE_PLATFORM_WINDOWS
+	return strerror_s(buf, bufsize, errnum);
+#else
+	return strerror_r(errnum, buf, bufsize);
+#endif
+}
