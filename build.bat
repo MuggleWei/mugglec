44974d2049f9e22142b0be6db23d62b3b0842695
Muggle Wei <mugglewei@gmail.com>
2022-10-10 10:37:50 +0800

    update build utility scripts, use ~/.local as install & find package path

--- build.bat
+++ build.bat
@@ -3,8 +3,9 @@
 setlocal ENABLEDELAYEDEXPANSION
 
 set origin_dir=%~dp0
+set user_local_dir=f:\\.local
 set build_dir=%origin_dir%build
-set install_dir=%origin_dir%build\pkg
+set install_dir=%user_local_dir%
 
 md %build_dir%
 cd %build_dir%
@@ -16,8 +17,10 @@ cmake .. ^
 	-DMUGGLE_BUILD_TESTING=OFF ^
 	-DMUGGLE_BUILD_BENCHMARK=OFF ^
 	-DMUGGLE_BUILD_TRACE=OFF ^
-	-DMUGGLE_EXTRA_PREFIX_PATH=f:/c_pkg ^
+	-DMUGGLE_EXTRA_PREFIX_PATH=%user_local_dir% ^
+	-DMUGGLE_INSTALL_BIN=OFF ^
 	-DCMAKE_INSTALL_PREFIX=%install_dir% ^
 	-G "Visual Studio 16 2019" -A x64
 
-+cmake --build . --config Release --target INSTALL
+:: cmake --build . --config Release --target INSTALL
+cmake --build . --config Release
--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 origin_dir="$(dirname "$(readlink -f "$0")")"
+user_local_dir=$HOME/.local
 build_dir=$origin_dir/build
-install_dir=$origin_dir/build/pkg
+install_dir=$user_local_dir
 
 mkdir -p $build_dir
 cd $build_dir
@@ -15,8 +16,9 @@ cmake \
 	-DMUGGLE_BUILD_TESTING=OFF \
 	-DMUGGLE_BUILD_BENCHMARK=OFF \
 	-DMUGGLE_BUILD_TRACE=OFF \
-	-DMUGGLE_EXTRA_PREFIX_PATH=~/c_pkg \
+	-DMUGGLE_EXTRA_PREFIX_PATH=$user_local_dir \
 	-DMUGGLE_INSTALL_BIN=OFF \
 	-DCMAKE_INSTALL_PREFIX=$install_dir
 
-make && make install
+# make && make install
+make
