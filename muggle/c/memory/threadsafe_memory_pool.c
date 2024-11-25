3b6cebddaf95b608bc7e63d4873eb05fb068bae7
Muggle Wei <mugglewei@gmail.com>
2024-11-25 14:58:40 +0800

    update threadsafe_memory_pool and sowr_memory_pool, use ROUND_UP_POW_OF_2_MUL to align x2 cache line

--- muggle/c/memory/threadsafe_memory_pool.c
+++ muggle/c/memory/threadsafe_memory_pool.c
@@ -33,12 +33,9 @@ int muggle_ts_memory_pool_init(muggle_ts_memory_pool_t *pool, muggle_sync_t capa
 	}
 
 	// align block_size to MUGGLE_CACHE_LINE_X2_SIZE
-	muggle_sync_t block_size = 
+	muggle_sync_t block_size =
 		(muggle_sync_t)sizeof(muggle_ts_memory_pool_head_t) + data_size;
-	muggle_sync_t quotient = block_size / MUGGLE_CACHE_LINE_X2_SIZE;
-	muggle_sync_t remainder = block_size % MUGGLE_CACHE_LINE_X2_SIZE;
-	block_size = quotient * MUGGLE_CACHE_LINE_X2_SIZE +
-		(remainder == 0 ? 0 : MUGGLE_CACHE_LINE_X2_SIZE);
+	block_size = ROUND_UP_POW_OF_2_MUL(block_size, MUGGLE_CACHE_LINE_X2_SIZE);
 	if (block_size <= 0)
 	{
 		return MUGGLE_ERR_INVALID_PARAM;
