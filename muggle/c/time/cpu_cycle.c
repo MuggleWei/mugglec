d3942fc0cad5d40fdbfcf2a5c976d0609b2917a4
Muggle Wei <mugglewei@gmail.com>
2025-01-13 13:14:39 +0800

    update cpu_cycle, support directly call rdtsc/rdtscp

--- muggle/c/time/cpu_cycle.c
+++ muggle/c/time/cpu_cycle.c
@@ -11,6 +11,7 @@
 #include "cpu_cycle.h"
 
 #if MUGGLE_PLATFORM_WINDOWS
+	// see: https://learn.microsoft.com/en-us/cpp/intrinsics/compiler-intrinsics
 	#include <intrin.h>
 #elif MUGGLE_PLATFORM_LINUX && defined(__x86_64__)
 	#include <x86intrin.h>
@@ -19,10 +20,40 @@
 
 uint64_t muggle_get_cpu_cycle()
 {
-#if MUGGLE_PLATFORM_WINDOWS
+#if MUGGLE_SUPPORT_RDTSC
 	return __rdtsc();
+#else
+	return 0;
+#endif
+}
+
+uint64_t muggle_rdtsc()
+{
+#if MUGGLE_PLATFORM_WINDOWS
+	#if defined(_M_IX86) || defined(_M_X64)
+	return __rdtsc();
+	#else
+	return 0;
+	#endif
 #elif MUGGLE_PLATFORM_LINUX && defined(__x86_64__)
 	return __rdtsc();
+#else
+	return 0;
+#endif
+}
+
+uint64_t muggle_rdtscp()
+{
+#if MUGGLE_PLATFORM_WINDOWS
+	#if defined(_M_IX86) || defined(_M_X64)
+	unsigned int tsc_aux;
+	return __rdtscp(&tsc_aux);
+	#else
+	return 0;
+	#endif
+#elif MUGGLE_PLATFORM_LINUX && defined(__x86_64__)
+	unsigned int tsc_aux;
+	return __rdtscp(&tsc_aux);
 #else
 	return 0;
 #endif
--- muggle/c/time/cpu_cycle.h
+++ muggle/c/time/cpu_cycle.h
@@ -16,6 +16,18 @@
 
 EXTERN_C_BEGIN
 
+#if MUGGLE_PLATFORM_WINDOWS
+	#if defined(_M_IX86) || defined(_M_X64)
+		#define MUGGLE_SUPPORT_RDTSC 1
+	#else
+		#define MUGGLE_SUPPORT_RDTSC 0
+	#endif
+#elif MUGGLE_PLATFORM_LINUX && defined(__x86_64__)
+	#define MUGGLE_SUPPORT_RDTSC 1
+#else
+	#define MUGGLE_SUPPORT_RDTSC 0
+#endif
+
 /**
  * @brief get cpu cycle
  *
@@ -23,6 +35,30 @@ EXTERN_C_BEGIN
  */
 MUGGLE_C_EXPORT
 uint64_t muggle_get_cpu_cycle();
+
+/**
+ * @brief rdtsc
+ *
+ * @return
+ *   on success, return cpu time Time Stamp Counter
+ *   otherwise return 0
+ *
+ * @NOTE only support in x86/x64, check MUGGLE_SUPPORT_RDTSC
+ */
+MUGGLE_C_EXPORT
+uint64_t muggle_rdtsc();
+
+/**
+ * @brief rdtscp
+ *
+ * @return
+ *   on success, return cpu time Time Stamp Counter
+ *   otherwise return 0
+ *
+ * @NOTE only support in x86/x64, check MUGGLE_SUPPORT_RDTSC
+ */
+MUGGLE_C_EXPORT
+uint64_t muggle_rdtscp();
 
 EXTERN_C_END
 
