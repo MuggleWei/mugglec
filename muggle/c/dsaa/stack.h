585e0d216009c0ab57a3e20bffd034a5c68ec497
Muggle Wei <mugglewei@gmail.com>
2026-05-21 14:16:19 +0800

    update stack
    - add NULL pointer check in destroy

--- muggle/c/dsaa/stack.h
+++ muggle/c/dsaa/stack.h
@@ -74,7 +74,7 @@ void muggle_stack_clear(muggle_stack_t *p_stack, muggle_dsaa_data_free func_free
 /**
  * @brief get number of elements in stack
  *
- * @param p_array_list   pointer to stack
+ * @param p_stack  pointer to stack
  *
  * @return number of elements in stack
  */
@@ -84,8 +84,8 @@ size_t muggle_stack_size(muggle_stack_t *p_stack);
 /**
  * @brief ensure capacity of allocated storage of stack 
  *
- * @param p_array_list  pointer to stack
- * @param capacity      capacity of allocated storage of stack
+ * @param p_stack   pointer to stack
+ * @param capacity  capacity of allocated storage of stack
  *
  * @return boolean
  */
@@ -95,8 +95,8 @@ bool muggle_stack_ensure_capacity(muggle_stack_t *p_stack, size_t capacity);
 /**
  * @brief push data into stack
  *
- * @param p_array_list  pointer to stack
- * @param data          pushed data
+ * @param p_stack  pointer to stack
+ * @param data     pushed data
  *
  * @return boolean
  */
@@ -106,7 +106,7 @@ muggle_stack_node_t* muggle_stack_push(muggle_stack_t *p_stack, void *data);
 /**
  * @brief get top node of stack
  *
- * @param p_array_list  pointer to stack
+ * @param p_stack  pointer to stack
  *
  * @return top node of stack, if stack is empty, return NULL
  */
@@ -116,9 +116,9 @@ muggle_stack_node_t* muggle_stack_top(muggle_stack_t *p_stack);
 /**
  * @brief pop top node of stack
  *
- * @param p_array_list  pointer to stack
- * @param func_free    function for free data, if it's NULL, do nothing for data
- * @param pool         the memory pool passed to func_free
+ * @param p_stack    pointer to stack
+ * @param func_free  function for free data, if it's NULL, do nothing for data
+ * @param pool       the memory pool passed to func_free
  */
 MUGGLE_C_EXPORT
 void muggle_stack_pop(muggle_stack_t *p_stack, muggle_dsaa_data_free func_free, void *pool);
