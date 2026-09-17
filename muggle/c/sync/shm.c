16b7f07fae5e570d9e0256a192090bca8a69a178
Muggle Wei <mugglewei@gmail.com>
2026-09-17 14:04:19 +0800

    fix muggle_shm_open in macos

--- muggle/c/sync/shm.c
+++ muggle/c/sync/shm.c
@@ -118,20 +118,22 @@ void *muggle_shm_open(muggle_shm_t *shm, const char *k_name, int k_num,
 		flag_privilege |= 0006;
 	}
 
+	#if MUGGLE_PLATFORM_LINUX
 	int flag_huge_section = flag & 0x00ff0000;
 	switch (flag_huge_section) {
 	case MUGGLE_SHM_FLAG_HUGE_DEFAULT: {
 		flag_huge = SHM_HUGETLB;
 	} break;
-	#if MUGGLE_C_HAVE_SHM_HUGE_NBYTES
+		#if MUGGLE_C_HAVE_SHM_HUGE_NBYTES
 	case MUGGLE_SHM_FLAG_HUGE_2MB: {
 		flag_huge = SHM_HUGETLB | SHM_HUGE_2MB;
 	} break;
 	case MUGGLE_SHM_FLAG_HUGE_1GB: {
 		flag_huge = SHM_HUGETLB | SHM_HUGE_1GB;
 	} break;
-	#endif
+		#endif
 	}
+	#endif
 
 	int shm_id =
 		shmget(shm_key, nbytes, flag_open | flag_privilege | flag_huge);
